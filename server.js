4db969b330b1f59f519086bf9cba5346bf134104
JackLian <hljw4@Hotmail.com>
2021-09-14 12:32:11 +1000

    Create basic hello world web application

---
+++ server.js
@@ -0,0 +1,14 @@
+const { hostname } = require('os');
+const http = require('http');
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain')
+    res.end("Hi\n");
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server listening on ${hostname}:${port}`);
+})
