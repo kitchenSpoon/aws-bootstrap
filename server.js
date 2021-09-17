b4a55872017d7abeb061784ecfcf434408bf2129
JackLian <hljw4@Hotmail.com>
2021-09-17 18:54:39 +1000

    Add ascii art

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const { hostname } = require('os');
 const http = require('http');
 const STACK_NAME = process.env.STACK_NAME || "Unknown Stack";
-const message = `Hello World from ${hostname()} in ${STACK_NAME}\n`;
+const content = `
+    _
+   | |
+___| |__   ___  ___ _ __
+/ __| '_ \ / _ \/ _ \ '_ \
+\__ \ | | |  __/  __/ |_) |
+|___/_| |_|\___|\___| .__/
+                | |
+                |_|
+`;
+const message = `Hello World from ${hostname()} in ${STACK_NAME}\n${content}`;
 const port = 8080;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
