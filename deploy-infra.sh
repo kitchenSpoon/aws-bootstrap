76dfbc8f3ff724644139f4c4d8ac1c307a21e54a
JackLian <hljw4@Hotmail.com>
2021-09-17 16:25:16 +1000

    Add a custom domain

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -10,6 +10,7 @@ AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
         --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
+DOMAIN=jacklian.net
 
 # Generate a personal access token with repo and admin:repo_hook
 #    permissions from https://github.com/settings/tokens
@@ -59,6 +60,7 @@ aws cloudformation deploy \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE \
+    Domain=$DOMAIN \
     GitHubOwner=$GH_OWNER \
     GitHubRepo=$GH_REPO \
     GitHubBranch=$GH_BRANCH \
